4032af76347e8d7542e8b893e43238c094551510
MRossello11 <miquel.rossello.mas@gmail.com>
2023-05-01 11:56:59 +0200

    Changed: Login screen

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,20 +1,19 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import core.ComeypagaStyles.appColors
 import core.Constants
-import feature_user.presentation.login.LoginController
-import feature_user.presentation.login.LoginScreen
 import core.service.createRetrofit
 import feature_user.data.UserRepositoryImpl
 import feature_user.data.data_source.UserDataSource
 import feature_user.domain.use_cases.LoginUseCase
 import feature_user.domain.use_cases.UserUseCases
+import feature_user.presentation.login.LoginController
+import feature_user.presentation.login.LoginScreen
 
 @Composable
 @Preview
@@ -27,21 +26,7 @@ fun App() {
         LoginController(useCases)
 
     MaterialTheme(
-        colors = Colors(
-            primary = Color(0xff33cc33),
-            onPrimary = Color(0xffffffff),
-            primaryVariant = Color(0x2FBC2F),
-            secondary = Color(0xff2FBCBC),
-            onSecondary = Color(0xffffffff),
-            secondaryVariant = Color(0xff07A59B),
-            background = Color(0xffffffff),
-            onBackground = Color(0xff000000),
-            isLight = false,
-            error = Color(0xffB00020),
-            onError = Color(0xff6e00ee),
-            surface = Color(0xffffffff),
-            onSurface = Color(0xff000000),
-        )
+        colors = appColors
     ) {
         LoginScreen(loginController)
     }
--- src/jvmMain/kotlin/feature_user/presentation/login/LoginScreen.kt
+++ src/jvmMain/kotlin/feature_user/presentation/login/LoginScreen.kt
@@ -1,18 +1,30 @@
 package feature_user.presentation.login
 
+import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.material.Button
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement.Bottom
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
+import core.ComeypagaStyles.appColors
+import core.ComeypagaStyles.spacerModifier
+import core.Constants
 import core.components.AppHeader
+import core.components.LabeledTextField
+import core.components.PrimaryButton
+import core.service.createRetrofit
+import feature_user.data.UserRepositoryImpl
+import feature_user.data.data_source.UserDataSource
+import feature_user.domain.use_cases.LoginUseCase
+import feature_user.domain.use_cases.UserUseCases
 
 @Composable
 fun LoginScreen(
@@ -31,45 +43,98 @@ fun LoginScreen(
 
     Column(
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
         AppHeader("Come y paga")
 
         Image(
             modifier = Modifier
-                .heightIn(30.dp, 60.dp)
-                .widthIn(30.dp, 60.dp),
+                .heightIn(150.dp, 200.dp)
+                .widthIn(150.dp, 200.dp),
             painter = painterResource("images/ComeYPaga.png"),
             contentDescription = "icon"
         )
 
-        TextField(
+        Spacer(modifier = spacerModifier)
+
+        LabeledTextField(
             value = user,
             onValueChange = {
                 user = it
                 loginController.onEvent(LoginEvent.UsernameEntered(it))
-            }
+            },
+            label = "Username"
         )
-        TextField(
+
+        Spacer(modifier = spacerModifier)
+
+        LabeledTextField(
             value = password,
             onValueChange = {
                 password = it
                 loginController.onEvent(LoginEvent.PasswordEntered(it))
-            }
+            },
+            label = "Password",
+            visualTransformation = PasswordVisualTransformation()
         )
 
-        Button(
-            onClick = {
-                loginController.onEvent(LoginEvent.Login)
-            },
-            content = {
-                Text(text = "Login")
-            }
-        )
+        Spacer(modifier = spacerModifier)
 
         Text(
-            text = "Return code: ${viewState.wsReturnCode}"
+            modifier = Modifier.clickable {
+                //todo: navigate to reset password screen
+                println("Reset password")
+            },
+            text = "Reset password",
+            textDecoration = TextDecoration.Underline,
+            color = appColors.primary
         )
+
+        Column(
+            modifier = Modifier
+                .width(200.dp)
+                .fillMaxHeight(),
+            verticalArrangement = Bottom
+        ) {
+            Spacer(modifier = spacerModifier)
+
+            PrimaryButton(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    // todo
+                    println("Create account")
+                },
+                content = "Create account"
+            )
+
+            PrimaryButton(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    loginController.onEvent(LoginEvent.Login)
+                },
+                content = "Login"
+            )
+            Spacer(modifier = spacerModifier)
+        }
+    }
+}
+
+@Composable
+@Preview
+fun PreviewLoginScreen(){
+    val useCases = UserUseCases(
+        loginUseCase = LoginUseCase(
+            UserRepositoryImpl(
+                createRetrofit(Constants.WebService.BASE_URL).create(
+                    UserDataSource::class.java))
+        )
+    )
+    val loginController =
+        LoginController(useCases)
+
+    MaterialTheme(appColors){
+        LoginScreen(loginController)
     }
 }
